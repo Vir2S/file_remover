51f3813e0b5e533de257839165acc0725f9cbf09
vir2s <virtualik@gmail.com>
2021-04-19 21:34:18 +0300

    error with dots in filenames fixed

--- file_remover.py
+++ file_remover.py
@@ -6,29 +6,42 @@ from watchdog.events import FileSystemEventHandler
 
 
 class Handler(FileSystemEventHandler):
+	folder = FOLDER
+	folder_track = FOLDER["tracking"]
+	folder_dest = FOLDER["destination"]
+	extension_track = EXTENSION
+
 	# If modified then moving files inside folder
 	def on_modified(self, event):
-		# Moving all files inside folder folder_track
-		for filename in os.listdir(folder_track):
+		# Moving all files inside tracking folder
+		for filename in os.listdir(self.folder_track):
 			# Check file extension
-			extension = filename.split(".")
+			try:
+				extension = filename.split(".")[-1]
+			except IndexError:
+				extension = ""
 			# If photo
-			if len(extension) > 1 and (extension[1].lower() == "jpg" or extension[1].lower() == "png" or extension[1].lower() == "svg"):
+			if len(extension) > 1 and extension.lower() in self.extension_track["photo"]:
 				# move file into photos folder
-				file = folder_track + "/" + filename
-				new_path = folder_dest + "/Photos/" + filename
+				file = self.folder_track + "/" + filename
+				new_path = self.folder_dest + "/Photos/" + filename
 				os.rename(file, new_path)
 			# If video then move into videos folder
-			elif len(extension) > 1 and extension[1].lower() == "mp4":
-				file = folder_track + "/" + filename
-				new_path = folder_dest + "/Videos/" + filename
+			elif len(extension) > 1 and extension.lower() in self.extension_track["video"]:
+				file = self.folder_track + "/" + filename
+				new_path = self.folder_dest + "/Videos/" + filename
+				os.rename(file, new_path)
+			# If pdf then move into pdfs folder
+			elif len(extension) > 1 and extension.lower() in self.extension_track["pdf"]:
+				file = self.folder_track + "/" + filename
+				new_path = self.folder_dest + "/Pdfs/" + filename
 				os.rename(file, new_path)
 
 
 # Start the program
 handle = Handler()
 observer = Observer()
-observer.schedule(handle, folder_track, recursive=True)
+observer.schedule(handle, FOLDER["tracking"], recursive=True)
 observer.start()
 
 # The code will checking every 10 milliseconds
@@ -38,4 +51,4 @@ try:
 except KeyboardInterrupt:
 	observer.stop()
 
-observer.join()
+observer.stop()
